2ee8dfdaa33cfeb60465d2afaafa843d4bd4dfa5
AmanMayank <aman.msingh87@gmail.com>
2023-06-16 22:32:55 +0530

    HeaderUIComplete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { Nav, Logo, NavMenu } from "../styles/Header";
+import { Nav, Logo, NavMenu, Login } from "../styles/Header";
 
 function Header() {
   return (
@@ -11,7 +11,34 @@ function Header() {
           <img src="/images/home-icon.svg" alt="home" />
           <span>Home</span>
         </a>
+
+        <a href="/home">
+          <img src="/images/search-icon.svg" alt="Search" />
+          <span>Search</span>
+        </a>
+
+        <a href="/home">
+          <img src="/images/watchlist-icon.svg" alt="Watchlist" />
+          <span>Watchlist</span>
+        </a>
+
+        <a href="/home">
+          <img src="/images/original-icon.svg" alt="Originals" />
+          <span>Originals</span>
+        </a>
+
+        <a href="/home">
+          <img src="/images/movie-icon.svg" alt="Movies" />
+          <span>Movies</span>
+        </a>
+
+        <a href="/home">
+          <img src="/images/series-icon.svg" alt="Series" />
+          <span>Series</span>
+        </a>
       </NavMenu>
+
+      <Login>Login</Login>
     </Nav>
   );
 }
