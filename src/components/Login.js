3fd55133bc8c9ec6f3b0d11739ec41cc0ee0569c
AmanMayank <aman.msingh87@gmail.com>
2023-06-16 20:12:35 +0530

    LoginPageBgImage

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Content, BgImage } from "../styles/Login";
+
+function Login() {
+  return (
+    <Container>
+      <Content>
+        <BgImage />
+      </Content>
+    </Container>
+  );
+}
+
+export default Login;
