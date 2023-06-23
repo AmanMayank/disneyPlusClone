731fc78dda4dd6426b66d8e49d9a9e611bb84b5b
AmanMayank <aman.msingh87@gmail.com>
2023-06-23 18:28:19 +0530

    Recommended Section Added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Container } from "../styles/Home";
 import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
+import Recommends from "./Recommends";
 
 function Home() {
   return (
     <Container>
       <ImgSlider />
       <Viewers />
+      <Recommends />
     </Container>
   );
 }
