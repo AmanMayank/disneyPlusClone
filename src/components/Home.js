b2e7c844e3a37a6208b09ae228d7e154d75e2dd6
AmanMayank <aman.msingh87@gmail.com>
2023-06-23 17:27:38 +0530

    Viewer component added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container } from "../styles/Home";
 import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
 
 function Home() {
   return (
     <Container>
       <ImgSlider />
+      <Viewers />
     </Container>
   );
 }
---
+++ src/components/Viewers.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Container, Wrap } from "../styles/Viewers";
+
+function Viewers() {
+  return (
+    <Container>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="disney" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+
+      <Wrap>
+        <img src="/images/viewers-pixar.png" alt="disney" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+
+      <Wrap>
+        <img src="/images/viewers-marvel.png" alt="disney" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+
+      <Wrap>
+        <img src="/images/viewers-starwars.png" alt="disney" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+
+      <Wrap>
+        <img src="/images/viewers-national.png" alt="disney" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source
+            src="/videos/1564676296-national-geographic.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </Wrap>
+    </Container>
+  );
+}
+
+export default Viewers;
